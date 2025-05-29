cfced9da5c5abedcb687ff38d11cad5ec03f38d9
Adriana Macedo <adri_mace@hotmail.com>
2025-05-29 21:56:26 +0100

    feat:criacao de spinner de estados de loading

--- src/components/TV/TVCard.tsx
+++ src/components/TV/TVCard.tsx
@@ -2,22 +2,21 @@ import { Link } from "react-router-dom";
 import type { TV } from "../../types/interfaces";
 import { poster } from "../../utils/imagePath";
 import StarRating from "../StarRating/StarRating";
+import "../../assets/styles/_common.scss";
 
 interface TVCardProps {
   tv: TV;
 }
 
 function TVCard({ tv }: TVCardProps) {
-  
   return (
     <Link to={`/tv/${tv.id}`}>
       <div key={tv.id} className="card">
         <img src={`${poster}${tv.poster_path || ""}`} alt={tv.name} />
         <h3 className="card-title">{tv.name}</h3>
         <div className="footer-items">
-
-        <p className="release-date">{tv.first_air_date.slice(0, 4)}</p>
-        <StarRating voteAverage={tv.vote_average} />
+          <p className="release-date">{tv.first_air_date.slice(0, 4)}</p>
+          <StarRating voteAverage={tv.vote_average} />
         </div>
       </div>
     </Link>
